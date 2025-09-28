06b1b8eda9b8ddc5d8b26af343239bc975722084
KOSKERZKY <dylanurex.x@gmail.com>
2025-09-28 00:51:49 -0500

    Funciones de Python.py

---
+++ Operaciones.py
@@ -0,0 +1,22 @@
+#Esto sera una lista de Operaciones utiles y una que otras muy escenciales
+#Obviamente con una breve explicacion
+
+print() # muestra algo en pantalla.
+
+input() # pide texto al usuario. un extre es decirte que todo lo que pones en input() se hace cadena de texto por mas que pongas un numero sera texto a menos que lo hagas numero entero con int()
+
+str() # convierte a texto.
+
+int() # convierte a número entero.
+
+float() # convierte a número con decimales.
+
+bool() # convierte a verdadero/falso.
+
+type() # dice el tipo de dato.
+
+round() # redondea un número.
+
+len() # da la longitud de listas, strings, etc.
+
+#Un dato a darte es que la funcion no funciona por si misma tienes que especificarle lo que quieres que haga la funcion con lo que pones dentro de los dos parentesis
