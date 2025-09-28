40a2ef60e5e704f4790ba52e974bff57dcdc0845
KOSKERZKY <dylanurex.x@gmail.com>
2025-09-28 00:17:43 -0500

    Variables.py

---
+++ Variable.py
@@ -0,0 +1,22 @@
+#Antes de empezar es de aclarar que una variable nunca puede tener un espacio en su nombre por ejemplo: 'var 0' lo correcto seria 'var_0' no necesariamente un (_) no importa lo que pongas tan solo se cumpla que no tenga la variable un espacio 
+#Empezaremos creando una variable similar al ejemplo anterior que sera 'var_1'
+#Utilizaremos esta estructura:
+
+#Nombre_de_variable = valor_de_la_variable
+var_0 = "Hub" #En este caso el tipo de dato que tiene la variable es una cadena de texto o str()
+print(var_0) #output = Hub
+#Aqui podemos cambiar el valor de una variable si le asignamos el mismo nombre pero con diferente valor
+var_0 = "Git" #output = Git
+#El valor a de 'var_0' a cambiado de "Hub" ➜ "Git" y podemos cambiar el valor de una variable las veces que deceemos
+#Pondre ejemplos de Variables con los varios tipos de datos
+
+string = "Dev"
+integer = 67
+decimal = 3.1416
+boolean = False
+
+#Tambien podemos poner el valor de una variable dentro la misma
+#Las veces que queramos
+#Aqui un ejemplo:
+var = var_0
+print(var) #output = Git
