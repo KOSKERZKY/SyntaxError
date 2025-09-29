c85c3031b4b49cf5870f3aab838389e216d5ba80
KOSKERZKY <dylanurex.x@gmail.com>
2025-09-29 14:44:47 -0500

    fix Funciones.py

--- Funciones.py
+++ Funciones.py
@@ -2,7 +2,7 @@
 #Puedes llamar una o mas palabras clave sin tener que escribirlas de nuevo solo llamando a la variable donde se encuentran
 #La palabra clave para crear una variable es definir una funcion es usar 'def'
 #Estructura: 
-def nombre_funcion(parametro) 
+def nombre_funcion(parametro): #los parametros son opcionales
   """cuerpo 
   de 
   la 
@@ -10,14 +10,14 @@ def nombre_funcion(parametro)
   return
 
 #Pondre un ejemplo
-def suma(a, b) #definimos una funcion y los parametros que se deben rellenar 
+def suma(a, b): #definimos una funcion y los parametros que se deben rellenar 
   return a + b #el resultado esta guardado en esta funcion cuando se llama  un dato es que return solo se puede usar en funciones
 
 s = suma(5 + 3) #Aqui llamamos a la funcion 'suma()' y le asignamos valores para a y b
 print(s) # 8 
 
 #Existe una manera de hacer que ahorrarce el poner print() cada que quieres mostrar el resultado y es poniendolo dentro de la funcion
-def resta(a, b) #
+def resta(a, b): #definimos una funcion y los parametros que se deben rellenar
   print(a - b) #imprime automaticamente cuando se llama la funcion
 
 resta(5, 3) #2
